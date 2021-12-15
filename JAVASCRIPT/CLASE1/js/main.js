e472671e3c8b7772b6ee71dcbd84f81a54eb1a48
Carlos Padilla <cpadillaortega@gmail.com>
2021-12-15 11:05:12 -0400

    initial commit

---
+++ JAVASCRIPT/CLASE1/js/main.js
@@ -0,0 +1,18 @@
+console.log(nombreVariable1);
+
+// Comentario de una linea 
+
+/* Comentario de mas lineas */
+
+// Declarar variable asi
+var nombreVariable1 = "Curso de Javascript" ;
+let nombreVariable2 = "Curso de programación";
+const nobreVariable3 = "coderhouse";
+
+let nombre = prompt ("ingrese su nombre")
+let espacio = (" ")
+
+console.log(" mi nombre es:" + espacio + nombre)
+
+
+alert("ola compa")
